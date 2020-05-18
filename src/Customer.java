81b8018cf5ed890ff43f71d3778a185b85530017
Rostyslav <rstq.brh@gmail.com>
2020-05-18 15:10:28 +0300

    Uml Java Bank

---
+++ src/Customer.java
@@ -0,0 +1,178 @@
+package Customer;
+
+
+/**
+* @generated
+*/
+public class Customer {
+    
+    /**
+    * @generated
+    */
+    private String firstName;
+    
+    /**
+    * @generated
+    */
+    private String lastName;
+    
+    /**
+    * @generated
+    */
+    private String phoneNumber;
+    
+    /**
+    * @generated
+    */
+    private Integer cardNumber;
+    
+    /**
+    * @generated
+    */
+    private String address;
+    
+    
+    /**
+    * @generated
+    */
+    private Account Account;
+    
+    
+    /**
+    * @generated
+    */
+    private String getFirstName() {
+        return this.firstName;
+    }
+    
+    /**
+    * @generated
+    */
+    private String setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+    /**
+    * @generated
+    */
+    private String getLastName() {
+        return this.lastName;
+    }
+    
+    /**
+    * @generated
+    */
+    private String setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    /**
+    * @generated
+    */
+    private String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+    
+    /**
+    * @generated
+    */
+    private String setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    
+    /**
+    * @generated
+    */
+    private Integer getCardNumber() {
+        return this.cardNumber;
+    }
+    
+    /**
+    * @generated
+    */
+    private Integer setCardNumber(Integer cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+    
+    /**
+    * @generated
+    */
+    private String getAddress() {
+        return this.address;
+    }
+    
+    /**
+    * @generated
+    */
+    private String setAddress(String address) {
+        this.address = address;
+    }
+    
+    
+    /**
+    * @generated
+    */
+    public Account getAccount() {
+        return this.Account;
+    }
+    
+    /**
+    * @generated
+    */
+    public Account setAccount(Account Account) {
+        this.Account = Account;
+    }
+    
+
+    //                          Operations                                  
+    
+    /**
+    * @generated
+    */
+    public Customer() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public Customer() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public getName() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public getAddress() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public setName() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public setAddress() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public getCardName() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public setCardName() {
+        //TODO
+    }
+    
+}
---
+++ src/Manager.java
@@ -0,0 +1,122 @@
+package Manager;
+
+
+/**
+* @generated
+*/
+public class Manager {
+    
+    /**
+    * @generated
+    */
+    private Integer idBankManager;
+    
+    /**
+    * @generated
+    */
+    private String firstName;
+    
+    /**
+    * @generated
+    */
+    private String position;
+    
+    /**
+    * @generated
+    */
+    private Integer idManager;
+    
+    
+    
+    /**
+    * @generated
+    */
+    private Integer getIdBankManager() {
+        return this.idBankManager;
+    }
+    
+    /**
+    * @generated
+    */
+    private Integer setIdBankManager(Integer idBankManager) {
+        this.idBankManager = idBankManager;
+    }
+    
+    /**
+    * @generated
+    */
+    private String getFirstName() {
+        return this.firstName;
+    }
+    
+    /**
+    * @generated
+    */
+    private String setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+    /**
+    * @generated
+    */
+    private String getPosition() {
+        return this.position;
+    }
+    
+    /**
+    * @generated
+    */
+    private String setPosition(String position) {
+        this.position = position;
+    }
+    
+    /**
+    * @generated
+    */
+    private Integer getIdManager() {
+        return this.idManager;
+    }
+    
+    /**
+    * @generated
+    */
+    private Integer setIdManager(Integer idManager) {
+        this.idManager = idManager;
+    }
+    
+    
+
+    //                          Operations                                  
+    
+    /**
+    * @generated
+    */
+    public Manager() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public getFirstNameFirstName() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public setFirstName() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public getPosition() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public setPosition() {
+        //TODO
+    }
+    
+}
